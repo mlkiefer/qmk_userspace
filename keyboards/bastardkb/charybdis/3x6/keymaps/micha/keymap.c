53656503c2179bf6c7a94226dfdaf5b0c605e3ea
Michael Kiefer <5708274+mlkiefer@users.noreply.github.com>
2024-12-02 16:24:21 +0100

    feat: drag on separate button and volume controls

--- keyboards/bastardkb/charybdis/3x6/keymaps/micha/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/micha/keymap.c
@@ -66,7 +66,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       KC_LGUI,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RGUI,
+       DRGSCRL,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RGUI,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        POINT,      GUI_A,   ALT_S,   SFT_D,   CTL_F,   KC_G,       KC_H,    CTL_J,   SFT_K,  ALT_L, GUI_QUOT,  POINT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
@@ -114,11 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_FUNC] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_PSCR,    KC_F7,  KC_F8,  KC_F9,  KC_F10, XXXXXXX,
+       XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_PSCR,    KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_VOLU,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_SCLN, KC_COLN, KC_LCBR, KC_LPRN, KC_LBRC,    KC_SCRL,    KC_F4,  KC_F5,  KC_F6,  KC_F11, XXXXXXX,
+       XXXXXXX, KC_SCLN, KC_COLN, KC_LCBR, KC_LPRN, KC_LBRC,    KC_SCRL,    KC_F4,  KC_F5,  KC_F6,  KC_F11, KC_VOLD,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_CIRC, KC_AMPR, KC_RCBR, KC_RPRN, KC_RBRC,    KC_PAUS,    KC_F1,  KC_F2,  KC_F3,  KC_F12, XXXXXXX,
+       XXXXXXX, KC_CIRC, KC_AMPR, KC_RCBR, KC_RPRN, KC_RBRC,    KC_PAUS,    KC_F1,  KC_F2,  KC_F3,  KC_F12, KC_MUTE,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                   KC_ESC,   KC_SPC, _______,    XXXXXXX, _______
   //                            ╰───────────────────────────╯ ╰──────────────────╯
